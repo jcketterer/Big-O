11f56b462ee4b921d633ccae1567adb12f549ee2
JC Ketterer <jcketterer18@gmail.com>
2022-05-17 23:17:23 -0700

    updated insertion sort

--- dsa-sorting/insertion.js
+++ dsa-sorting/insertion.js
@@ -1,9 +1,27 @@
-function insertionSort(arr) {
+function insertionSort(array) {
+  const arr = array.slice()
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      if (arr[j] < arr[j - 1]) {
+        // const temp = arr[j]
+        // arr[j] = arr[j - 1]
+        // arr[j - 1] = temp
+        ;[arr[j], arr[j - 1]] = [arr[j - 1], arr[j]]
+      } else {
+        break
+      }
+    }
+  }
+  return arr
+}
+
+function insertionSortWhileLoop(arr) {
   for (let i = 1; i < arr.length; i++) {
     let currentValue = arr[i]
+
     let j = i - 1
 
-    while (j > -1 && currentValue < arr[j]) {
+    while (arr[j] > currentValue) {
       arr[j + 1] = arr[j]
       j--
     }
@@ -12,6 +30,6 @@ function insertionSort(arr) {
   return arr
 }
 
-console.log(insertionSort([0, -10, 7, 4]))
+console.log(insertionSortWhileLoop([0, -10, 7, 4]))
 
 module.exports = insertionSort
